ea924f073acf84c3400f785fafec664041b37d40
Vybhav K Acharya <63064905+vybhav-acharya@users.noreply.github.com>
2021-11-17 15:27:42 +0530

    Made teachers work. Learners still broken

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from helpers import login_required, apology
 from flask_session import Session
 import sys
+import os
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+# from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
+
 
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:Imawesome@localhost/company'
 
@@ -14,6 +17,7 @@ db = SQLAlchemy(app)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+# Session = session(bind = engine)
 
 class Company(db.Model):
   __tablename__ = 'company'
@@ -56,6 +60,7 @@ class Employee(db.Model):
     salary=db.Column(db.Float())
     companyid=db.Column(db.Integer,primary_key=True)
     departmentid=db.Column(db.Integer,primary_key=True)
+    
     
 
     def __init__(self,ssn,name,employeeid,address,phone_number,job,salary,companyid,departmentid):
@@ -100,12 +105,13 @@ class Teaching(db.Model):
     __tablename__ = 'teaching'
 
     teacher_rating= db.Column(db.Integer)
-    teacher_ssn= db.Column(db.String(),primary_key=True)
+    teacher_ssn= db.Column(db.String(), primary_key=True, nullable=False)
     tech_name=db.Column(db.String())
     techid= db.Column(db.Integer,primary_key=True)
     companyid=db.Column(db.Integer,primary_key=True)
     departmentid=db.Column(db.Integer,primary_key=True)
-	
+    
+    
 
     def __init__(self, teacher_rating,teacher_ssn,tech_name,techid,companyid, departmentid):
       self.techid=techid
@@ -291,8 +297,13 @@ def technologies():
 @app.route('/classes')
 @login_required
 def classes():
-	teachers = Teaching.query.filter(Teaching.companyid == session["companyid"])
-
+	# teachers=Teaching.query.join(Employee,Employee.ssn==Teaching.teacher_ssn).filter
+	# teachers = Teaching.query.join(Employee)\
+	# 					.add_columns(Teaching.techid, Teaching.teacher_rating, Teaching.teacher_ssn, Teaching.tech_name, Teaching.companyid, Teaching.departmentid, Employee.name)\
+	# 					.filter(Teaching.companyid == session["companyid"])
+	t1=Teaching.query.filter(Teaching.companyid == session["companyid"])
+	teachers = db.session.execute("select * from teaching inner join employee on teaching.teacher_ssn = employee.ssn where Teaching.companyid=:id", {'id': session["companyid"]})
+	# teachers = Teaching.query.from_statement(db.text("select * from teaching inner join employee on teaching.teacher_ssn = employee.ssn where Teaching.companyid=:id").params(id=session["companyid"]))
 	teacher_result = [
     {
 		"techid": teacher.techid,
@@ -300,7 +311,8 @@ def classes():
 		"teacher_ssn": teacher.teacher_ssn,
 		"tech_name": teacher.tech_name,
 		"companyid": teacher.companyid,
-		"departmentid":  teacher.departmentid
+		"departmentid":  teacher.departmentid,
+		"tname": teacher.name
     }
     for teacher in teachers]
 # 1
